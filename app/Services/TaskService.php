2267899d6f18084cabc3dd332f8a255f88e6757b
Danilo Oliveira <daniloworkdev@gmail.com>
2024-01-15 16:10:42 -0300

    feat(services): add TaskService for task filtering and pagination

---
+++ app/Services/TaskService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Task;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class TaskService
+{
+    protected $task;
+
+    public function __construct(Task $task)
+    {
+        $this->task = $task;
+    }
+
+    /**
+     * Apply filters to a query builder instance.
+     *
+     * @param Builder $query The query builder instance.
+     * @param array $filters The filters to apply.
+     * @return Builder The modified query builder instance.
+     */
+    public function applyFilters(Builder $query, array $filters): Builder
+    {
+        $filterMap = [
+            'status'   => 'status',
+            'priority' => 'priority',
+            'user'     => 'user_id',
+        ];
+
+        foreach ($filterMap as $filterKey => $dbColumn) {
+            if (isset($filters[$filterKey])) {
+                $query->where($dbColumn, $filters[$filterKey]);
+            }
+        }
+
+        if (isset($filters['deadline_start']) && isset($filters['deadline_end'])) {
+            $query->whereBetween('deadline', [
+                $filters['deadline_start'],
+                $filters['deadline_end'] . Carbon::now()->endOfDay()->toDateTimeString(),
+            ]);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Retrieves a paginated list of tasks based on specified filters.
+     *
+     * @param array $filters The filters to be applied to the query.
+     * @param int $perPage The number of tasks to be displayed per page.
+     * @param int $page The current page number.
+     * @throws Some_Exception_Class Description of the exception that can be thrown.
+     * @return LengthAwarePaginator The paginated tasks.
+     */
+    public function getPaginatedTasks(array $filters, int $perPage, int $page): LengthAwarePaginator
+    {
+        $query = $this->task->newQuery();
+        $this->applyFilters($query, $filters);
+
+        return $query->paginate($perPage, ['*'], 'page', $page);
+    }
+}
