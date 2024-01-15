d4676a4c2ac4640d0ad981c21517b0be8eed479e
Danilo Oliveira <daniloworkdev@gmail.com>
2024-01-15 16:08:57 -0300

    feat(requests): add UserLoginRequest for login validation

---
+++ app/Http/Requests/UserLoginRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\DefaultMessages;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserLoginRequest extends FormRequest
+{
+    use DefaultMessages;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => [
+                'required',
+                'string',
+                'email'
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:6'
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return $this->defaultMessage();
+    }
+}
