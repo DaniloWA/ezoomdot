c0ce7e92f75172bca56d2e74a590f338bd735c23
Danilo Oliveira <daniloworkdev@gmail.com>
2024-01-16 17:42:27 -0300

    refactor(routes): update routes file structure, add tasks-related routes, remove duplicate auth inclusion

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/tasks', [TasksController::class, 'index'])->name('tasks.index');
+    Route::post('/tasks/store', [TasksController::class, 'store'])->name('task.store');
+    Route::get('/tasks/{task}', [TasksController::class, 'show'])->name('task.show');
+    Route::get('/tasks/users', [TasksController::class, 'getUsers'])->name('tasks.users');
+    Route::patch('/tasks', [TasksController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{uuid}', [TasksController::class, 'destroy'])->name('tasks.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
